e0bc3beb00f5e44a2620dab07c47aecdddf44b21
junko0126 <hikari2011h.j@gmail.com>
2020-08-18 09:44:02 +0900

    コンフリクトの修正２

--- Gemfile
+++ Gemfile
@@ -64,5 +64,9 @@ group :production do
   gem 'rails_12factor'
 end
 
+
 gem 'devise'
 gem 'pry-rails'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
+
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
-  def new
+
+  def index
   end
+  
+  def new
+    @item = Item.new
 
   def create
     @item = Item.create(item_params)
@@ -13,6 +17,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item)
+    params.require(:item).permit(:name, :content, :image, :price, :category, :status, :payment, :place, :days).marge(user_id: crrent_user.id)
   end
 end
