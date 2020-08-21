f12158bc8e214e6349a49d19e4e261f38547d3f3
junko0126 <hikari2011h.j@gmail.com>
2020-08-21 21:53:25 +0900

    PR修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,17 @@
 class ItemsController < ApplicationController
-
   def index
   end
-  
+
   def new
     @item = Item.new
   end
 
   def create
-    # binding.pry
     @item = Item.new(item_params)
     if @item.save
-       redirect_to root_path
+      redirect_to root_path
     else
-       render :new
+      render :new
     end
   end
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-        name                 { "あああああ" }
-        content              { Faker::Lorem.sentence }
-        category             { Category.all.sample }
-        status               { Status.all.sample }
-        payment              { Payment.all.sample }
-        prefecture           { Prefecture.all.sample }
-        days                 { Days.all.sample }
-        price                { "299" }
-        association :user   
+    name { 'あああああ' }
+    content              { Faker::Lorem.sentence }
+    category             { Category.all.sample }
+    status               { Status.all.sample }
+    payment              { Payment.all.sample }
+    prefecture           { Prefecture.all.sample }
+    days                 { Days.all.sample }
+    price                { '1000' }
+    association :user
   end
 end
