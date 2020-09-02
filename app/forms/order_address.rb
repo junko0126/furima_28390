d5d16638ebe2c7e04beb36e52e26aeb6c7a2f850
junko0126 <hikari2011h.j@gmail.com>
2020-09-02 16:34:22 +0900

    バリデーション修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -2,12 +2,13 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :token, :user_id, :item_id, :prefecture_id, :postal_code, :city, :house_number, :building, :phone, :order_id
 
+  limit1 = /\A[ぁ-んァ-ン一-龥]+\z/
   POSTAL_CODE = /\A[0-9]{3}-[0-9]{4}\z/.freeze
   VALID_PHONE = /\A\d{10}$|^\d{11}\z/.freeze
 
   with_options presence: true do
     validates :prefecture_id, :house_number
-    validates :city, format: { with: @limit1, message: 'is invalid. Input full-width characters.' }
+    validates :city, format: { with: limit1, message: 'is invalid. Input full-width characters.' }
     validates :postal_code, format: { with: POSTAL_CODE, message: 'is invalid. Include hyphen(-)' }
     validates :phone, format: { with: VALID_PHONE, message: 'is invalid. Maxmam 11' }
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   has_many :orders
 
   # "正規表現"
-  @limit1 = /\A[ぁ-んァ-ン一-龥]+\z/
+  limit1 = /\A[ぁ-んァ-ン一-龥]+\z/
   limit2 = /\A[ァ-ヶー－]+\z/
   limit3 = /\A[a-z0-9]+\z/i
   limit4 = /\A[a-z\d{6,20}[@]{3,20}\z]/i
   limit5 = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i
 
   with_options presence: true do
-    validates :first_name, :family_name, format: { with: @limit1, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, :family_name, format: { with: limit1, message: 'is invalid. Input full-width characters.' }
     validates :first_name_kana, :family_name_kana, format: { with: limit2, message: 'is invalid. Input full-width katakana characters.' }
     validates :nickname, format: { with: limit3, message: 'is invalid. Input half-width characters.' }
     validates :email, format: { with: limit4, message: 'is invalid.' }
