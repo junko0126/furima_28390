372c533929c97d30f92be1ee681ce8c3d4d32f1c
junko0126 <hikari2011h.j@gmail.com>
2020-09-03 12:03:08 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
   # "正規表現"
   limit1 = /\A[ぁ-んァ-ン一-龥]+\z/
   limit2 = /\A[ァ-ヶー－]+\z/
-  limit3 = /\A[a-z0-9]+\z/i
+  limit3 = /\A[[a-z0-9][ぁ-んァ-ン一-龥]]+\z/
   limit4 = /\A[a-z\d{6,20}[@]{3,20}\z]/i
   limit5 = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i
 
   with_options presence: true do
     validates :first_name, :family_name, format: { with: limit1, message: 'is invalid. Input full-width characters.' }
     validates :first_name_kana, :family_name_kana, format: { with: limit2, message: 'is invalid. Input full-width katakana characters.' }
-    validates :nickname, format: { with: limit3, message: 'is invalid. Input half-width characters.' }
+    validates :nickname, format: { with: limit3, message: 'is invalid.' }
     validates :email, format: { with: limit4, message: 'is invalid.' }
     validates :password, format: { with: limit5, message: 'is invalid.' }
     validates :birthday
